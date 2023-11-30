a034e7d0d0a0ca68705b4b6d0acb9560fd10ed87
Alan Pugachev <aszneesizzle@gmail.com>
2023-11-30 15:02:01 +0300

    init

---
+++ src/main/java/com/alanpugachev/spring_microservices/SpringMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.alanpugachev.spring_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanpugachev/spring_microservices/SpringMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpugachev.spring_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
